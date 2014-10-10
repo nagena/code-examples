dc18eba872bfc6a5a032df72e33e698c2c81b296
Horacio Casatti <horacio.casatti@mercadolibre.com>
2014-10-10 14:20:17 -0300

    custom checkout steps

---
+++ customized-checkout/payment/bash/create_payment.sh
@@ -0,0 +1,14 @@
+curl -X POST \
+	-H 'accept: application/json' \
+	-H 'content-type: application/json' \
+	https://api.mercadolibre.com/checkout/custom/create_payment?access_token=tu_access_token \
+	-d '{
+		"amount": 10,
+		"reason": "Título de lo que están pagando",
+		"currency_id": "ARS",
+		"installments": 3,
+		"payment_method_id": "visa",
+		"card_token_id": "card_token",
+		"payer_email": "payer@email.com",
+		"external_reference": "1234"
+	}'
