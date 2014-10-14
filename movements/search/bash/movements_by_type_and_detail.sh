fd9a2d8d3490130b328beb89ddc2ab4a40813238
Horacio Casatti <horacio.casatti@mercadolibre.com>
2014-10-14 14:49:25 -0300

    movements search

--- movements/search/bash/movements_by_type_and_detail.sh
+++ movements/search/bash/movements_by_type_and_detail.sh
@@ -1,3 +1,3 @@
 curl -X GET \
 -H 'accept: application/json' \
-'https://api.mercadolibre.com/mercadopago_account/movements/search?access_token=your_access_token&type=tipo-de-movimiento&detail=movement_detail
+'https://api.mercadolibre.com/mercadopago_account/movements/search?access_token=your_access_token&type=movement_type&detail=movement_detail
